789884b46b9c2d22d41473774207227681ceaaa2
deni.wintaka <15612267+denicahyawintaka@users.noreply.github.com>
2024-10-09 16:35:24 +0800

    first commit

---
+++ app/src/main/java/com/example/videocallapp/component/HelloDev.kt
@@ -0,0 +1,12 @@
+package com.example.videocallapp.component
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun HelloDev(
+    modifier: Modifier = Modifier
+) {
+    Text(text = "Halooo Dev")
+}
